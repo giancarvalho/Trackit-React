6b8ac4f1ed4eb31421ae9edd61c9be968415c278
giancarvalho <giandcarvalho@gmail.com>
2021-09-30 19:24:45 -0300

    exported getFormatedDate function

---
+++ src/scripts/getFormatedDate.js
@@ -0,0 +1,8 @@
+export default function getFormatedDate(day) {
+    day = day ? day : new Date();
+
+    let options = { weekday: "long", month: "numeric", day: "numeric" };
+    let date = day.toLocaleString("pt-BR", options);
+
+    return date.charAt(0).toUpperCase() + date.slice(1);
+}
