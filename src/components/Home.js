b3d64097138b76522bb5710da5c42b16fb2c607c
giancarvalho <giandcarvalho@gmail.com>
2021-10-02 12:17:33 -0300

    moved local storage retrieval function to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,85 +1,97 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { loginRequest } from "../trackitRequests";
 import {
-  Anchor,
-  LoginContainer,
-  LogoContainer,
-  Main,
+    Anchor,
+    LoginContainer,
+    LogoContainer,
+    Main,
 } from "./shared/stylesFrontPages";
 import { SubmitButton, Input } from "./shared/stylesApp";
 import { useHistory } from "react-router";
 import UserContext from "../contexts/UserContext";
 
 export default function Home() {
-  const [email, setEmail] = useState("");
-  const [disabled, setDisabled] = useState(false);
-  const [password, setPassword] = useState("");
-  const history = useHistory();
-  const { user, setUser } = useContext(UserContext);
+    const [email, setEmail] = useState("");
+    const [disabled, setDisabled] = useState(false);
+    const [password, setPassword] = useState("");
+    const history = useHistory();
+    const { user, setUser } = useContext(UserContext);
 
-  function storeUser(userData) {
-    localStorage.setItem("storedUser", JSON.stringify(userData));
-  }
+    useEffect(() => {
+        const storedUser = getStoredUser();
 
-  if (user) {
-    history.push("/hoje");
-  }
+        if (storedUser) {
+            setUser(storedUser);
+            history.push("/hoje");
+        }
+    }, []);
 
-  function LogIn(e) {
-    e.preventDefault();
-    setDisabled(true);
-    let body = {
-      email,
-      password,
-    };
+    function getStoredUser() {
+        let storedUser = localStorage.getItem("storedUser");
+        storedUser = JSON.parse(storedUser);
 
-    loginRequest(body)
-      .then((response) => {
-        setUser(response.data);
-        storeUser(response.data);
-        history.push("/hoje");
-      })
-      .catch((error) => {
-        alert("Confira novamente suas credenciais");
-        setDisabled(false);
-      });
-  }
+        return storedUser;
+    }
 
-  return (
-    <Main>
-      <LogoContainer>
-        <img src={logo} alt="logo-TrackIt" />
-      </LogoContainer>
-      <LoginContainer>
-        <form onSubmit={LogIn}>
-          <fieldset disabled={disabled}>
-            <Input
-              placeholder="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input
-              type="password"
-              placeholder="senha"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <SubmitButton
-              type="submit"
-              width="100%"
-              height="45px"
-              disabled={disabled}
-            >
-              Entrar
-            </SubmitButton>
-          </fieldset>
-        </form>
-      </LoginContainer>
-      <Link to="/cadastro">
-        <Anchor>Não tem uma conta? Cadastre-se!</Anchor>
-      </Link>
-    </Main>
-  );
+    function storeUser(userData) {
+        localStorage.setItem("storedUser", JSON.stringify(userData));
+    }
+
+    function LogIn(e) {
+        e.preventDefault();
+        setDisabled(true);
+        let body = {
+            email,
+            password,
+        };
+
+        loginRequest(body)
+            .then((response) => {
+                setUser(response.data);
+                storeUser(response.data);
+                history.push("/hoje");
+            })
+            .catch((error) => {
+                alert("Confira novamente suas credenciais");
+                setDisabled(false);
+            });
+    }
+
+    return (
+        <Main>
+            <LogoContainer>
+                <img src={logo} alt="logo-TrackIt" />
+            </LogoContainer>
+            <LoginContainer>
+                <form onSubmit={LogIn}>
+                    <fieldset disabled={disabled}>
+                        <Input
+                            placeholder="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <Input
+                            type="password"
+                            placeholder="senha"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <SubmitButton
+                            type="submit"
+                            width="100%"
+                            height="45px"
+                            disabled={disabled}
+                        >
+                            Entrar
+                        </SubmitButton>
+                    </fieldset>
+                </form>
+            </LoginContainer>
+            <Link to="/cadastro">
+                <Anchor>Não tem uma conta? Cadastre-se!</Anchor>
+            </Link>
+        </Main>
+    );
 }
