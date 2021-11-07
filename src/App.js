f77e1ae0afc2f4fc041925df541b52afb4da53de
giancarvalho <giandcarvalho@gmail.com>
2021-11-07 00:42:30 -0300

    fixed bug where deleting a habit woulndt update the todayList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "../styles/reset.css";
-import "../styles/global.css";
+import "./styles/reset.css";
+import "./styles/global.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Home from "../views/Home";
-import SignUp from "../views/SignUp";
-import Today from "../views/Today";
+import Home from "./views/Home";
+import SignUp from "./views/SignUp";
+import Today from "./views/Today";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useState } from "react";
-import UserContext from "../contexts/UserContext";
-import History from "../views/History";
-import Habits from "../views/Habits";
-import PrivateRoute from "../routes/privateRoute";
+import UserContext from "./contexts/UserContext";
+import History from "./views/History";
+import Habits from "./views/Habits";
+import PrivateRoute from "./routes/privateRoute";
 
 function App() {
     const [user, setUser] = useState({ token: null });
